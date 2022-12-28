52591fe9cdebec5522abf6e96380669451b1effe
roeyrach <93585090+roeyrach@users.noreply.github.com>
2022-12-28 15:08:27 +0200

    server on air

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const cors = require("cors")
 
 const mongoose = require("mongoose")
 
-const PORT = 3000
+const uri =
+	"mongodb+srv://guyroey:X3PErZ1DUnqfGWee@cluster0.xcovqzy.mongodb.net/?retryWrites=true&w=majority"
+
+const PORT = process.env.PORT || 8081
+
+mongoose
+	.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((res) => console.log("connected"))
+	.catch((err) => console.log(err))
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
